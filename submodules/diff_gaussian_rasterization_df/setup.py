b0b3aa71353f6da828290a42719cb902a3c1d45d
YueqiWeng <weng.yq97@gmail.com>
2026-06-21 01:13:21 -0400

    r

--- submodules/diff_gaussian_rasterization_df/setup.py
+++ submodules/diff_gaussian_rasterization_df/setup.py
@@ -10,25 +10,38 @@
 #
 
 from setuptools import setup
-from torch.utils.cpp_extension import CUDAExtension, BuildExtension
 import os
-os.path.dirname(os.path.abspath(__file__))
+
+_here = os.path.dirname(os.path.abspath(__file__))
+
+ext_modules = []
+cmdclass = {}
+
+try:
+    from torch.utils.cpp_extension import CUDAExtension, BuildExtension
+    have_torch = True
+except Exception:
+    have_torch = False
+
+if have_torch and os.environ.get('CUDA_HOME'):
+    ext_modules = [
+        CUDAExtension(
+            name="diff_gaussian_rasterization_df._C",
+            sources=[
+                os.path.join(_here, "cuda_rasterizer/rasterizer_impl.cu"),
+                os.path.join(_here, "cuda_rasterizer/forward.cu"),
+                os.path.join(_here, "cuda_rasterizer/backward.cu"),
+                os.path.join(_here, "rasterize_points.cu"),
+                os.path.join(_here, "ext.cpp")],
+            extra_compile_args={"nvcc": ["-I" + os.path.join(_here, "third_party/glm/")]})
+    ]
+    cmdclass = {'build_ext': BuildExtension}
+else:
+    print("Warning: CUDA extension build skipped. Ensure 'torch' is installed and 'CUDA_HOME' is set to build the native extensions.")
 
 setup(
     name="diff_gaussian_rasterization_df",
     packages=['diff_gaussian_rasterization_df'],
-    ext_modules=[
-        CUDAExtension(
-            name="diff_gaussian_rasterization_df._C",
-            sources=[
-            "cuda_rasterizer/rasterizer_impl.cu",
-            "cuda_rasterizer/forward.cu",
-            "cuda_rasterizer/backward.cu",
-            "rasterize_points.cu",
-            "ext.cpp"],
-            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]})
-        ],
-    cmdclass={
-        'build_ext': BuildExtension
-    }
+    ext_modules=ext_modules,
+    cmdclass=cmdclass,
 )
